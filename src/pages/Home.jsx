550c6499661d2ab051b673c7ffaf1e95c319429a
Yudha Kurniawan Lumban Gaol <yudha.personals@gmail.com>
2023-06-05 07:01:45 +0700

    add routes, components, and, pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import About from './About';
+import Contact from './Contact';
+import Project from './Project';
+
+const Home = () => {
+  return (
+    <>
+      <About />
+      <Contact />
+      <Project />
+    </>
+  );
+};
+
+export default Home;
